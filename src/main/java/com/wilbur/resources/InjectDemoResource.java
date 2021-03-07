f739e6dcdbb7656ca0ee3c93d473746c1dfc6e67
Jonathas Wilbur <=>
2021-03-07 07:45:48 -0300

    Matrix, Header, Cookie - InjectDemo

--- src/main/java/com/wilbur/resources/InjectDemoResource.java
+++ src/main/java/com/wilbur/resources/InjectDemoResource.java
@@ -1,5 +1,20 @@
 package com.wilbur.resources;
 
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Path("/injectdemo")
+@Produces(MediaType.TEXT_PLAIN)
+@Consumes(MediaType.TEXT_PLAIN)
 public class InjectDemoResource
 {
+    @GET
+    @Path("annotations")
+    public String getParamsUsingAnnotations(@MatrixParam("param") String matrixParam,
+                                            @HeaderParam("headerParam") String headerParam,
+                                            @CookieParam("cookieParam") String cookieParam)
+    {
+        return String.format("matrixParam: %s \nheaderParam: %s \ncookieParam: %s",
+                                matrixParam, headerParam, cookieParam);
+    }
 }
